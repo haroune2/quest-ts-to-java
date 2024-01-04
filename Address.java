005304e9082e32a12012d368e45b84964e41f91e
boumedjirek <haroun902009@hotmail.fr>
2024-01-04 02:12:13 +0100

    TS TO JAVA

---
+++ Address.java
@@ -0,0 +1,12 @@
+public class Address {
+    
+
+    private String number; 
+    private String street; 
+    private String city; 
+    private String country; 
+
+
+
+
+}
---
+++ User.java
@@ -0,0 +1,10 @@
+// User.java
+import java.util.Date;
+import java.util.List;
+
+public class User {
+    public Date birthdate;
+    public int size;
+    public List<Photo> photos;
+    public Address address;
+}
