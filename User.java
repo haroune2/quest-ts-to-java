0e2fb9b68e9ec87d002945285b4717a0b8027f68
boumedjirek <haroun902009@hotmail.fr>
2024-01-04 02:12:59 +0100

    ADDING SOME COMMENTS

--- User.java
+++ User.java
@@ -2,6 +2,9 @@
 import java.util.Date;
 import java.util.List;
 
+//javac Address.java Photo.java User.java
+//javac User.java
+
 public class User {
     public Date birthdate;
     public int size;
